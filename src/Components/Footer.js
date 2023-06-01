f43b00253a3c3639e71da8061642bc5a3d5e24c6
pavansuresh <pavan.suresh23@gmail.com>
2023-06-01 10:39:25 +0530

    portfolio section updated.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 function Footer() {
   return (
-    <Box as="footer" py={4} bg="gray.200">
+    <Box as="footer" py={4} bg="gray.200"  mt={{ base: '20px', md: '50px' }}>
       <Flex justifyContent="center">
         <Text fontSize="sm" color="gray.500">
           &copy; {new Date().getFullYear()} PavanKumar
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -15,11 +15,25 @@ import {
   Button
 } from '@chakra-ui/react';
 
-import image1 from '../Assets/portfolio1.png';
+// thumbnail images
+import thumb_image1 from '../Assets/tumbnail4.jpg';
+import thumb_image2 from '../Assets/tumbnail2.jpg';
+import thumb_image3 from '../Assets/tumbnail3.jpg';
+import thumb_image4 from '../Assets/tumbnail1.jpg';
+import thumb_image5 from '../Assets/tumbnail5.jpg';
+import thumb_image6 from '../Assets/tumbnail6.jpg';
+import thumb_image7 from '../Assets/tumbnail7.jpg';
+import thumb_image8 from '../Assets/tumbnail8.jpg';
+
+// modal images
+import image1 from '../Assets/portfolio4.jpg';
 import image2 from '../Assets/portfolio2.jpg';
 import image3 from '../Assets/portfolio3.jpg';
-import image4 from '../Assets/portfolio4.jpg';
+import image4 from '../Assets/portfolio1.png';
 import image5 from '../Assets/portfolio5.jpg';
+import image6 from '../Assets/portfolio6.png';
+import image7 from '../Assets/portfolio7.jpg';
+import image8 from '../Assets/portfolio8.png';
 
 const Portfolio = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -28,29 +42,52 @@ const Portfolio = () => {
   const images = [
     {
       id: 1,
+      thumbscr: thumb_image1,
       src: image1,
       alt: 'Image 1',
     },
     {
       id: 2,
+      thumbscr: thumb_image2,
       src: image2,
       alt: 'Image 2',
     },
     {
       id: 3,
+      thumbscr: thumb_image3,
       src: image3,
       alt: 'Image 3',
     },
     {
       id: 4,
+      thumbscr: thumb_image4,
       src: image4,
       alt: 'Image 4',
     },
     {
       id: 5,
+      thumbscr: thumb_image5,
       src: image5,
       alt: 'Image 5',
-    }
+    },
+    {
+      id: 6,
+      thumbscr: thumb_image6,
+      src: image6,
+      alt: 'Image 6',
+    },
+    {
+      id: 7,
+      thumbscr: thumb_image7,
+      src: image7,
+      alt: 'Image 7',
+    },
+    {
+      id: 8,
+      thumbscr: thumb_image8,
+      src: image8,
+      alt: 'Image 8',
+    },
   ];
 
   const handleImageClick = (image) => {
@@ -88,7 +125,7 @@ const Portfolio = () => {
         {images.map((image) => (
           <Box key={image.id}>
             <Image
-              src={image.src}
+              src={image.thumbscr}
               alt={image.alt}
               width="100%"
               height="550px"
